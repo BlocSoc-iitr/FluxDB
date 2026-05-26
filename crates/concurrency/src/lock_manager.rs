473001423681e7abafd5c054e3a18b9b0a3335fc
Abhivansh <31abhivanshj@gmail.com>
2026-05-27 00:14:13 +0530

    chore: add initial struct definitions

--- crates/concurrency/src/lock_manager.rs
+++ crates/concurrency/src/lock_manager.rs
@@ -1,1 +1,25 @@
-// TODO
+use std::{
+    collections::{HashMap, VecDeque},
+    sync::Condvar,
+};
+
+enum LockType {
+    SharedLock,
+    ExclusiveLock,
+}
+
+struct LockRequest {
+    transaction_id: u64,
+    lock_type: LockType,
+    is_granted: bool,
+}
+
+struct LockQueue {
+    queue: VecDeque<LockRequest>,
+    cond_var: Condvar,
+    upgrading_flag: bool,
+}
+
+pub struct LockTable {
+    map: HashMap<u64, LockQueue>,
+}
