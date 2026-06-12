63b316ad94f238894016f89c053720122911a990
Abhivansh <31abhivanshj@gmail.com>
2026-06-13 02:44:42 +0530

    fix the next in WalIterator

--- crates/storage/src/wal.rs
+++ crates/storage/src/wal.rs
@@ -34,17 +34,41 @@ use common::WalError;
 pub type Result<T> = std::result::Result<T, WalError>;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum WalEntryType {
-    Put = 0,
-    Delete = 1,
+pub enum WalRecordType {
+    Insert = 0,
+    SetXMax = 1,
+    Commit = 2,
+    Abort = 3,
+    LeafSplit = 4,
+    InternalSPlit = 5,
+    InsertDownLink = 6,
+    NewRoot = 7,
+    PageAllocate = 8,
+    PageCompact = 9,
+    MarkHalfDead = 10,
+    UnlinkPage = 11,
+    Checkpoint = 12,
+    Fpi = 13,
 }
 
-impl TryFrom<u8> for WalEntryType {
+impl TryFrom<u8> for WalRecordType {
     type Error = WalError;
     fn try_from(value: u8) -> Result<Self> {
         match value {
-            0 => Ok(WalEntryType::Put),
-            1 => Ok(WalEntryType::Delete),
+            0 => Ok(WalRecordType::Insert),
+            1 => Ok(WalRecordType::SetXMax),
+            2 => Ok(WalRecordType::Commit),
+            3 => Ok(WalRecordType::Abort),
+            4 => Ok(WalRecordType::LeafSplit),
+            5 => Ok(WalRecordType::InternalSPlit),
+            6 => Ok(WalRecordType::InsertDownLink),
+            7 => Ok(WalRecordType::NewRoot),
+            8 => Ok(WalRecordType::PageAllocate),
+            9 => Ok(WalRecordType::PageCompact),
+            10 => Ok(WalRecordType::MarkHalfDead),
+            11 => Ok(WalRecordType::UnlinkPage),
+            12 => Ok(WalRecordType::Checkpoint),
+            13 => Ok(WalRecordType::Fpi),
             _ => Err(WalError::InvalidEntryType(value)),
         }
     }
@@ -63,7 +87,7 @@ struct Block {
 struct WalRecord {
     pub lsn: Lsn,
     pub rec_len: u32,
-    pub log_type: u8,
+    pub entry_type: WalRecordType,
     pub nblocks: u8,
     pub txn_id: u64, 
     pub main_len: u16,
@@ -85,9 +109,11 @@ pub struct Wal {
 }
 
 impl Iterator for WalIterator {
-    type Item = Result<WalEntry>;
+    type Item = Result<WalRecord>;
 
     fn next(&mut self) -> Option<Self::Item> {
+        let mut hasher = Hasher::new();
+
         let mut lsn_buf = [0u8; 8];
         if let Err(e) = self.reader.read_exact(&mut lsn_buf) {
             if e.kind() == io::ErrorKind::UnexpectedEof {
@@ -97,47 +123,131 @@ impl Iterator for WalIterator {
         }
         let lsn = Lsn::from_le_bytes(lsn_buf);
 
+        hasher.update(&lsn_buf);
+
+        let mut rec_len_buf = [0u8; 4];
+        if let Err(e) = self.reader.read_exact(&mut rec_len_buf) {
+            if e.kind() == io::ErrorKind::UnexpectedEof {
+                return None;
+            }
+            return Some(Err(e.into()))
+        } 
+        let rec_len = u32::from_le_bytes(rec_len_buf);
+
+        hasher.update(&rec_len_buf);
+
         let mut type_buf = [0u8; 1];
         if let Err(e) = self.reader.read_exact(&mut type_buf) {
             return Some(Err(e.into()));
         }
-        let entry_type = match WalEntryType::try_from(type_buf[0]) {
+        let entry_type = match WalRecordType::try_from(type_buf[0]) {
             Ok(t) => t,
             Err(e) => return Some(Err(e)),
         };
 
-        let mut key_len_buf = [0u8; 8];
-        if let Err(e) = self.reader.read_exact(&mut key_len_buf) {
+        hasher.update(&type_buf);
+
+        let mut nblocks_buf = [0u8; 1];
+        if let Err(e) = self.reader.read_exact(&mut nblocks_buf) {
             return Some(Err(e.into()));
         }
-        let key_len = u64::from_le_bytes(key_len_buf);
+        let nblocks = nblocks_buf[0];
 
-        let mut value_len_buf = [0u8; 8];
-        if let Err(e) = self.reader.read_exact(&mut value_len_buf) {
+        hasher.update(&nblocks_buf);
+
+        let mut txn_id_buf = [0u8; 8];
+        if let Err(e) = self.reader.read_exact(&mut txn_id_buf) {
             return Some(Err(e.into()));
         }
-        let value_len = u64::from_le_bytes(value_len_buf);
+        let txn_id = u64::from_le_bytes(txn_id_buf);
 
-        let mut timestamp_buf = [0u8; 8];
-        if let Err(e) = self.reader.read_exact(&mut timestamp_buf) {
+        hasher.update(&txn_id_buf);
+
+        let mut main_len_buf = [0u8; 2];
+        if let Err(e) = self.reader.read_exact(&mut main_len_buf) {
             return Some(Err(e.into()));
         }
-        let timestamp = u64::from_le_bytes(timestamp_buf);
+        let main_len = u16::from_le_bytes(main_len_buf);
 
-        let mut key = vec![0u8; key_len as usize];
-        if let Err(e) = self.reader.read_exact(&mut key) {
-            return Some(Err(e.into()));
-        }
+        hasher.update(&main_len_buf);
 
-        let value = if value_len > 0 {
-            let mut val_buf = vec![0u8; value_len as usize];
-            if let Err(e) = self.reader.read_exact(&mut val_buf) {
+        let mut blocks: Vec<Block> = Vec::with_capacity(nblocks as usize);
+
+        for _ in 0..nblocks {
+            let mut page_id_buf = [0u8; 8];
+            if let Err(e) = self.reader.read_exact(&mut page_id_buf) {
                 return Some(Err(e.into()));
             }
-            Some(val_buf)
+            let page_id = PageId::from_le_bytes(page_id_buf);
+
+            hasher.update(&page_id_buf);
+
+            let mut blk_flags_buf = [0u8; 1];
+            if let Err(e) = self.reader.read_exact(&mut blk_flags_buf) {
+                return Some(Err(e.into()));
+            }
+            let blk_flags = blk_flags_buf[0];
+            
+            hasher.update(&blk_flags_buf);
+
+            let mut data_len_buf = [0u8; 2];
+            if let Err(e) = self.reader.read_exact(&mut data_len_buf) {
+                return Some(Err(e.into()));
+            }
+            let data_len = u16::from_le_bytes(data_len_buf);
+
+            hasher.update(&data_len_buf);
+
+            let fpi = if blk_flags & 1 == 1 {
+                let mut fpi_buf = [0u8; PAGE_SIZE];
+                if let Err(e) = self.reader.read_exact(&mut fpi_buf) {
+                    return Some(Err(e.into()));
+                }
+                Some(fpi_buf)
+            } else {
+                None
+            };
+
+            if let Some(ref fpi_buf) = fpi {
+                hasher.update(fpi_buf);
+            }
+
+            let data = if data_len > 0 {
+                let mut data_buf = vec![0u8; data_len as usize];
+                if let Err(e) = self.reader.read_exact(&mut data_buf) {
+                    return Some(Err(e.into()));
+                }
+                Some(data_buf)
+            } else {
+                None
+            };
+
+            if let Some(ref data_buf) = data {
+                hasher.update(data_buf);
+            }
+
+            blocks.push(Block {
+                page_id,
+                blk_flags,
+                data_len,
+                fpi,
+                data,
+            });
+        }
+
+        let main_data = if main_len > 0 {
+            let mut main_data_buf = vec![0u8; main_len as usize];
+            if let Err(e) = self.reader.read_exact(&mut main_data_buf) {
+                return Some(Err(e.into()));
+            }
+            Some(main_data_buf)
         } else {
             None
         };
+
+        if let Some(ref main_data_buf) = main_data {
+            hasher.update(main_data_buf);
+        }
 
         let mut checksum_buf = [0u8; 4];
         if let Err(e) = self.reader.read_exact(&mut checksum_buf) {
@@ -145,16 +255,7 @@ impl Iterator for WalIterator {
         }
         let expected_checksum = u32::from_le_bytes(checksum_buf);
 
-        let mut hasher = Hasher::new();
-        hasher.update(&lsn_buf);
-        hasher.update(&type_buf);
-        hasher.update(&key_len_buf);
-        hasher.update(&value_len_buf);
-        hasher.update(&timestamp_buf);
-        hasher.update(&key);
-        if let Some(ref v) = value {
-            hasher.update(v);
-        }
+
         let actual_checksum = hasher.finalize();
 
         if actual_checksum != expected_checksum {
@@ -165,12 +266,15 @@ impl Iterator for WalIterator {
             }));
         }
 
-        Some(Ok(WalEntry {
+        Some(Ok(WalRecord {
             lsn,
+            rec_len,
             entry_type,
-            key,
-            value,
-            timestamp,
+            nblocks,
+            txn_id,
+            main_len,
+            blocks,
+            main_data: main_data.unwrap(),
         }))
     }
 }
