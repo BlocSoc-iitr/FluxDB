2fc509c5ac1b304c96c4dac737659c094d1bfacb
yash <yasha4658@gmail.com>
2026-06-24 12:53:28 +0530

    add proptest

--- crates/engine/src/lib.rs
+++ crates/engine/src/lib.rs
@@ -1,6 +1,9 @@
 mod engine;
 mod ops;
 mod txn;
+
+#[cfg(test)]
+mod proptests;
 pub use common::{BufferPoolError, DiskError, EngineError, IndexError, WalError};
 pub use common::{Key, Value};
 pub use engine::Engine;
